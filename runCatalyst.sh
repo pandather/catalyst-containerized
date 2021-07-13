b9b3ca5bf8a0a71508fe94e200f7dc1a99cbf3c5
Mark J. Kuebel <markk@riseup.net>
2021-07-13 05:57:57 +0000

    Fixed that bug that would probably wipe your system.

--- runCatalyst.sh
+++ runCatalyst.sh
@@ -3,6 +3,7 @@
 CATALYST_CONTAINER_CACHE_DIR=${CATALYST_CONTAINER_CACHE_DIR:=/var/tmp/catalyst-container/cache}
 CATALYST_CONTAINER_DISTFILES_CACHE_DIR=${CATALYST_CONTAINER_DISTFILES_CACHE_DIR:=$CATALYST_CONTAINER_CACHE_DIR/distfiles}
 CATALYST_CONTAINER_BINPKGS_CACHE_DIR=${CATALYST_CONTAINER_DISTFILES_CACHE_DIR:=$CATALYST_CONTAINER_CACHE_DIR/binpkgs}
+CATALYST_CONTAINER_PORTDIR=${CATALYST_CONTAINER_PORTDIR:=/var/tmp/catalyst-container/repos/gentoo}
 CATALYST_CACHE_DIR=${CATALYST_CACHE_DIR:=/var/tmp/catalyst}
 CATALYST_BUILDS_DIR=${CATALYST_BUILDS_DIR:=$CATALYST_CACHE_DIR/builds}
 CATALYST_SNAPSHOTS_DIR=${CATALYST_SNAPSHOTS_DIR:=$CATALYST_CACHE_DIR/snapshots}
@@ -16,19 +17,6 @@ CONTAINER_BUILD_DIR=${CONTAINER_BUILD_DIR:=$PWD}
 if [[ -z "${SPEC}" ]]; then
         echo "No spec specified"
         exit 1
-fi
-
-if [[ -z $PORTDIR ]] && [[ -f /etc/portage/make.conf ]]
-then
-    mkdir tmp
-    echo '#!/bin/sh' > tmp/buildEnv.sh
-    chmod a+x tmp/buildEnv.sh
-    cat /etc/portage/make.conf | grep '^PORTDIR=' >> tmp/buildEnv.sh
-    source ${PWD}/tmp/buildEnv.sh
-    rm tmp/buildEnv.sh
-    rmdir tmp
-else
-    PORTDIR=/var/tmp/catalyst-container/repos/gentoo
 fi
 
 mkdir -p ${CATALYST_CONTAINER_DISTFILES_CACHE_DIR} ${CATALYST_CONTAINER_BINPKGS_CACHE_DIR} ${CATALYST_BUILDS_DIR}/default ${CATALYST_PACKAGES_DIR} ${CATALYST_SNAPSHOTS_DIR} ${BUILDSH_SCRIPT_DIR} ${PORTDIR}
@@ -45,7 +33,7 @@ ${PWD}/build.sh \
 				 --mount type=bind,source=${CATALYST_BUILDS_DIR}/,target=/var/tmp/catalyst/builds/ \
 				 --mount type=bind,source=${CATALYST_SNAPSHOTS_DIR}/,target=/var/tmp/catalyst/snapshots/ \
 				 --mount type=bind,source=${CATALYST_PACKAGES_DIR}/,target=/var/tmp/catalyst/packages/ \
-				 --mount type=bind,source=${PORTDIR}/,target=/var/db/repos/gentoo/ \
+				 --mount type=bind,source=${CATALYST_CONTAINER_PORTDIR}/,target=/var/db/repos/gentoo/ \
                                  --mount type=bind,source=/proc/,target=/proc/ \
                                  --mount type=bind,source=/dev/,target=/dev/ \
                                  --mount type=bind,source=/sys/,target=/sys/ \
