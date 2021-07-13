4699a9174a3f45a5ebc1a0dd76040aca90a2ce16
Mark J. Kuebel <markk@riseup.net>
2021-07-13 03:23:03 +0000

    fixed so build catalyst starts confirmed, should complete build

--- container-script.sh
+++ container-script.sh
@@ -7,9 +7,9 @@ emerge --usepkg --buildpkg dev-util/catalyst net-dns/bind-tools dev-vcs/git app-
 
 libtool --finish /usr/lib64
 
-if [[ -f /var/tmp/catalyst/snapshots/gentoo-$(date +%Y%m%d).tar.bz2 ]];
+if [[ -f /var/tmp/catalyst/snapshots/gentoo-$(date +%Y-%m-%d).tar.bz2 ]];
 then
-    catalyst -s $(date +%Y%m%d)
+    catalyst -s $(date +%Y-%m-%d)
 fi
 
-catalyst -f /var/tmp/catalyst-container/scripts/toExec.spec
+catalyst -f /root/toExec.spec
